2005bcde6527f3754857119e22222866269c3376
Maria <81220388+mariagryp@users.noreply.github.com>
2023-10-16 12:11:47 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+/* Navbar */
+const menu = document.querySelector(".menu");
+const header = document.querySelector(".header");
+
+menu.addEventListener("click", () => {
+  header.classList.toggle("change");
+  menu.classList.toggle("change");
+});
+/* End of Navrba */
