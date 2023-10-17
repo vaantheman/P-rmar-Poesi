c20bd465a22559359e3e85e3554c559d97ea68f0
Maria <81220388+mariagryp@users.noreply.github.com>
2023-10-17 17:14:16 +0200

    Add files via upload

--- app.js
+++ app.js
@@ -12,13 +12,20 @@ let lengthItems = items.length - 1;
 /* Navbar */
 const menu = document.querySelector(".menu");
 const header = document.querySelector(".header");
+const allLinks = document.querySelectorAll(".nav-link");
 
 menu.addEventListener("click", () => {
   header.classList.toggle("change");
   menu.classList.toggle("change");
 });
-/* End of Navrba */
 
+allLinks.forEach((link) => {
+  link.addEventListener("click", () => {
+    header.classList.toggle("change");
+  });
+});
+
+/* End of Navrba */
 
 next.onclick = function () {
   if (active + 1 > lengthItems) {
