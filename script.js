c550846ba50f73906935e93a74b94256688f2b76
Maria <81220388+mariagryp@users.noreply.github.com>
2023-10-17 17:26:15 +0200

    Delete script.js

--- script.js
+++
@@ -1,9 +0,0 @@
-/* Navbar */
-const menu = document.querySelector(".menu");
-const header = document.querySelector(".header");
-
-menu.addEventListener("click", () => {
-  header.classList.toggle("change");
-  menu.classList.toggle("change");
-});
-/* End of Navrba */
