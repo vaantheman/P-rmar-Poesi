b835d336d81f7442d242b2daa46b08e2f815e591
Svintoo <S_maskuniitty@hotmail.se>
2023-10-19 09:42:34 +0200

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,15 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["*.{html,js}"],
+    corePlugins:[
+    'backgroundColor',
+    'borderColor',
+    'borderRadius',
+    'borderWidth',
+    'cursor',
+    'height',
+    'margin'
+      ],
   theme: {
     extend: {},
   },
